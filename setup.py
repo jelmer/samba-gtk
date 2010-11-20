c59d8c8ad3977b9c069e539277551f48194b583a
Jelmer Vernooij <jelmer@samba.org>
2010-11-20 17:20:50 +0100

    Add setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    version="0.0.1",
+    name='samba-gtk',
+    packages=[
+        'sambagtk',
+    ],
+    maintainer='Jelmer Vernooij',
+    maintainer_email='jelmer@samba.org',
+)
